a8e37f585615a9b9b3de88f11f89766e37172007
hongsh1023 <hsungho1023@gmail.com>
2020-10-05 17:25:49 +0900

    Revert "민국수정"
    This reverts commit 3d5b6205d833973820b7641c89db8e47a0ff9b1f.

--- Project_JavaKing/src/com/bit/dao/ClassRegDao.java
+++ Project_JavaKing/src/com/bit/dao/ClassRegDao.java
@@ -9,7 +9,6 @@ import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
 import com.bit.db.ConnectionProvider;
 import com.bit.vo.ClassregVo;
